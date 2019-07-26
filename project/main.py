69d6ec86523f1aa1740120e542a3c7fa1fcee121
angelikajeziorska <angjeziorska@wp.pl>
2019-07-26 19:29:08 +0200

    Kivy setup completed

--- project/main.py
+++ project/main.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+
+
 def main():
     from project.menu import choice
     from project.pickle_data import load_data
-    from project.plant_list import store_data
+    from project.pickle_data import store_data
     from project.pickle_data import clear_file
 
     list_of_plants = []
