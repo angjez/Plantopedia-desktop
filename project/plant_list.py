d65aed2a2901a3921adaa8582dc441773abdfd25
angelikajeziorska <angjeziorska@wp.pl>
2019-07-27 11:11:10 +0200

    ListOfPlants methods changed to static methods

--- project/plant_list.py
+++ project/plant_list.py
@@ -4,10 +4,12 @@ from project.plant_def import Plant
 class ListOfPlants:
     list_of_plants = []
 
+    @staticmethod
     def add_new_plant(list_of_plants):
         new_plant = Plant(input("Common name: "), input("Botanical name: "), input("Sun exposure: "), input("Water: "), input("Soil: "), input("Repotting: "), input("Size: "))
         list_of_plants.append(new_plant)
 
+    @staticmethod
     def print_list(list_of_plants):
         if list_of_plants:
             print("Your plants: ")
@@ -18,6 +20,7 @@ class ListOfPlants:
         else:
             print("You have no saved plants.")
 
+    @staticmethod
     def delete_from_list (list_of_plants, to_delete):
         for plant in list_of_plants:
             if plant.common_name == to_delete:
