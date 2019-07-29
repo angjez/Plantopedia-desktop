817d1bbb61a835f389d267153aef51fdbd522e03
angelikajeziorska <angjeziorska@wp.pl>
2019-07-29 15:07:04 +0200

    Menu screen added

--- project/kivy_interface/interface_main.py
+++ project/kivy_interface/interface_main.py
@@ -4,6 +4,7 @@ kivy.require('1.11.0')
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 class MenuBoxes(BoxLayout):
@@ -12,11 +13,22 @@ class MenuBoxes(BoxLayout):
         self.orientation = "horizontal"
 
         add_button = Button(text="Add", size_hint=(.1, .1))
+
         delete_button = Button(text="Delete", size_hint=(.1, .1))
+
         edit_button = Button(text="Edit", size_hint=(.1, .1))
-        self.add_widget(add_button)
-        self.add_widget(delete_button)
-        self.add_widget(edit_button)
+
+        for but in [add_button, delete_button, edit_button]:
+            self.add_widget(but)
+
+    def event_add_plant(self, obj):
+        print("Typical event from", obj)
+
+    def event_delete_plant(self, obj):
+        print("Typical event from", obj)
+
+    def event_edit_plant(self, obj):
+        print("Typical event from", obj)
 
 
 class PlantBoxes(BoxLayout):
@@ -24,12 +36,14 @@ class PlantBoxes(BoxLayout):
     def __init__(self, list_of_plants, **kwargs):
         super(PlantBoxes, self).__init__(**kwargs)
         self.orientation = "vertical"
-
         button = []
 
         for n in range(len(list_of_plants.list)):
             button.append(Button(text=list_of_plants.list[n].common_name))
             self.add_widget(button[n])
+
+    def event_show_plant(self, obj):
+        print("Typical event from", obj)
 
 
 class MainBoxes(BoxLayout):
@@ -43,6 +57,13 @@ class MainBoxes(BoxLayout):
         self.add_widget(menu_boxes)
 
 
+class MenuScreen(Screen):
+    def __init__(self,list_of_plants, **kwargs):
+        super(MenuScreen, self).__init__(**kwargs)
+        menu_page = MainBoxes(list_of_plants)
+        self.add_widget(menu_page)
+
+
 class MainApp(App):
     def build(self):
         from project.pickle_data import load_data
@@ -50,13 +71,16 @@ class MainApp(App):
         from project.pickle_data import clear_file
         from project.plant_list import ListOfPlants
 
+        self.title = "Plantopedia"
+
         list_of_plants = ListOfPlants()
         load_data(list_of_plants)
-
+        sm = ScreenManager()
+        sm.add_widget(MenuScreen(list_of_plants, name='Menu'))
         clear_file()
         store_data(list_of_plants.list)
 
-        return MainBoxes(list_of_plants)
+        return sm
 
 
 if __name__ == '__main__':
