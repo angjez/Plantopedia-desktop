f986112a38c35ccf21e7d9d8c6c494f6f7d20258
angelikajeziorska <angjeziorska@wp.pl>
2019-08-21 13:03:48 +0200

    Improved input labels and fields

--- project/kivy_interface/templates.py
+++ project/kivy_interface/templates.py
@@ -1,22 +1,14 @@
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.config import Config
+from kivy.uix.textinput import TextInput
 
 
 Config.set('graphics', 'width', '650')
 Config.set('graphics', 'height', '700')
 
 
-class PropertyLabel(Label):
-    def on_size(self, *args):
-        self.size_hint = (1.0, 1.0)
-        self.color = (0, 0, 0, 0.65)
-        self.halign = "left"
-        self.valign = "middle"
-        self.padding_x = 50
-        self.bind(size=self.setter("text_size"))
-        self.markup = True
-        self.font_size = self.height / 4.5
+# buttons
 
 
 class PlantButton(Button):
@@ -29,11 +21,28 @@ class PlantButton(Button):
         self.markup = True
         self.font_size = self.height / 3
         self.background_normal = "button.png"
+        self.background_down = "button_down.png"
 
 
 class MenuButton(Button):
     def on_size(self, *args):
         self.background_normal = "menu_button.png"
+        self.background_down = "button_down.png"
+
+
+# labels
+
+
+class PropertyLabel(Label):
+    def on_size(self, *args):
+        self.size_hint = (1.0, 1.0)
+        self.color = (0, 0, 0, 0.65)
+        self.halign = "left"
+        self.valign = "middle"
+        self.padding_x = 50
+        self.bind(size=self.setter("text_size"))
+        self.markup = True
+        self.font_size = self.height / 4.5
 
 
 class PlantLabel(Label):
@@ -57,3 +66,12 @@ class InputLabel(Label):
         self.padding_x = 50
         self.bind(size=self.setter("text_size"))
         self.markup = True
+        self.font_size = self.height / 4
+
+
+# text input
+class InputField(TextInput):
+    def on_size(self, *args):
+        self.background_color = (0.5, 0.5, 0.5, 0.3)
+        self.multiline = True
+        self.hint_text_color = (0, 0, 0, 0.5)
